72a5ab74c87b34ba1d5e24ac85027b925c4e45fa
devbit4 <devbit4@gmail.com>
2021-09-18 19:10:08 +0900

    20210918/ function exercise

---
+++ exercise5/script.js
@@ -0,0 +1,32 @@
+
+
+// if (Number(age) < 18) {
+// 	alert("Sorry, you are too yound to drive this car. Powering off");
+// } else if (Number(age) > 18) {
+// 	alert("Powering On. Enjoy the ride!");
+// } else if (Number(age) === 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// }
+
+// function checkDriverAge() {
+//     var age = prompt("what's your age?")
+//     if (Number(age) < 18) {
+//         alert("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         alert("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+function checkDriverAge(age) {
+
+    if (Number(age) < 18) {
+        return "Sorry, you are too yound to drive this car. Powering off";
+    } else if (Number(age) > 18) {
+        return "Powering On. Enjoy the ride!";
+    } else if (Number(age) === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    }
+
+}
