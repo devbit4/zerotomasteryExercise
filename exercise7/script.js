18bf8019f2b5d978ada16e3b71dbd75a4e870b77
devbit4 <devbit4@gmail.com>
2021-09-18 19:08:58 +0900

    20210918/ objects and arrays exercise

---
+++ exercise7/script.js
@@ -0,0 +1,21 @@
+var database [
+    username : "bitna" ,
+    password : "1234"
+
+]
+var newsfeed [
+    {
+        username : "sun" ,
+        timeline : "I'm happy"
+    } ,
+    {
+        username : "star" ,
+        timeline : "I'm sad"
+    } ,
+    {
+        username : "lunar" ,
+        timeline : "I'm fine"
+    }
+
+
+]
